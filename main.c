b4bb7505dd62862b8aca902dcb4c6e20093e0907
Nabih ALI <naali@student.42.fr>
2018-12-09 15:13:09 +0100

    Test de la video set

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 13:08:32 by naali             #+#    #+#             */
-/*   Updated: 2018/12/09 13:16:36 by naali            ###   ########.fr       */
+/*   Updated: 2018/12/09 14:28:17 by naali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "mlx.h"
+
+int		deal_key(int key, void *ptr)
+{
+	if (ptr != (void*)53)
+		printf("%d\n, %s", key, ptr);
+	else
+		exit(0);
+	return (0);
+}
 
 int		main()
 {
@@ -20,6 +31,7 @@ int		main()
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 800, 600, "Test");
 	mlx_pixel_put(mlx_ptr, win_ptr, 400, 300, 0xFF00FF);
+	mlx_key_hook(win_ptr, deal_key, (void*)0);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
