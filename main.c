341c937d08a23cde5a90ab357e663830f91a0029
Nabih ALI <naali@student.42.fr>
2018-12-09 15:48:57 +0100

    test creation de carree

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 13:08:32 by naali             #+#    #+#             */
-/*   Updated: 2018/12/09 14:28:17 by naali            ###   ########.fr       */
+/*   Updated: 2018/12/09 15:46:42 by naali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "mlx.h"
@@ -25,24 +26,31 @@ int		deal_key(int key, void *ptr)
 
 int		main()
 {
+	float		x;
+	float		y;
 	void	*mlx_ptr;
 	void	*win_ptr;
 
+	x = 0;
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 800, 600, "Test");
-	mlx_pixel_put(mlx_ptr, win_ptr, 400, 300, 0xFF00FF);
+	while (x < 800)
+	{
+		y = 0;
+		while (y < 600)
+		{
+			if ((x <= 400 + 50 && x >= 400 - 50) && (y == 300 + 50 || y == 300 - 50))
+				mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFF00FF);
+			if ((y <= 300 + 50 && y >= 300 - 50) && (x == 400 + 50 || x == 400 - 50))
+				mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x00FF00);
+			y++;
+		}
+		x = x + 1;
+	}
 	mlx_key_hook(win_ptr, deal_key, (void*)0);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
-
-
-
-
-
-
-
-
 
 
 
